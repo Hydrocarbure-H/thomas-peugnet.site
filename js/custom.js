64e8571b869b03799ac65e62fff2095576da057e
Thomas PEUGNET <thomas.peugnet.pro@gmail.com>
2022-11-16 23:40:23 +0100

    First commit

---
+++ js/custom.js
@@ -0,0 +1,16 @@
+// templatemo 467 easy profile
+
+// PRELOADER
+
+// $(window).load(function(){
+//     $('.preloader').delay(1000).fadeOut("slow"); // set duration in brackets    
+// });
+
+// // HOME BACKGROUND SLIDESHOW
+// $(function(){
+//     jQuery(document).ready(function() {
+// 		$('body').backstretch([
+// 			 "images/background_color_00004.png"
+// 	 			], 	{duration: 7000, fade: 1300});
+// 		});
+// })
